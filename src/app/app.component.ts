9afbec6c5b7f658ddf5ffc1b97d76a46add61965
dweep119 <dweeppatel123@gmail.com>
2021-11-02 22:31:54 +0530

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,56 @@
+import { Component, ViewChild } from '@angular/core';
+//@ts-ignore
+require('@silvermine/videojs-chromecast')(videojs, { preloadWebComponents: true });
+
+declare var videojs : any ;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'video-js';
+
+  @ViewChild('channelvideoTemplate') public channelvideoTemplate: any;
+
+  public player: any;
+
+ constructor() { }
+
+ ngOnInit(): void {
+   
+   var playerElement = document.getElementById("video-player");
+   //console.log("channel url", this.streamDetails[0].stream_design.channel_video_btn_url);
+
+   this.player = videojs(playerElement, {
+     techOrder: ["chromecast", "html5"],
+     autoplay: true,
+     muted: false,
+     loop: false,
+     fluid: true,
+     controls: true,
+     preload: "auto",
+     chromecast: {
+     },
+     sources: [{
+       type: "application/x-mpegURL",
+       src: "https://storefreestyle.teleosmedia.com/vod/freestyle/AngieLostGirlsTRAILER2020/playlist.m3u8"
+     }],
+     plugins: {
+       chromecast: {
+         receiverAppID: '646E7AC8',
+         addButtonToControlBar: true,
+       },
+      //  airPlay: {
+      //    addButtonToControlBar: true,
+      //  }
+     }
+   })
+   //@ts-ignore
+   window.SILVERMINE_VIDEOJS_CHROMECAST_CONFIG = {
+     preloadWebComponents: true,
+   };
+ }
+
+}
